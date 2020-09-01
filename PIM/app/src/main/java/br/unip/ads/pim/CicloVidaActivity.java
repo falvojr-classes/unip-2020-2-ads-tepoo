281b8befd95b61aa24057d4dd70b37616711ff44
Venilton FalvoJr <falvojr@gmail.com>
2020-09-01 20:35:53 -0300

    Aula 01/09

---
+++ PIM/app/src/main/java/br/unip/ads/pim/CicloVidaActivity.java
@@ -0,0 +1,53 @@
+package br.unip.ads.pim;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CicloVidaActivity extends AppCompatActivity {
+
+    private static final String TAG = CicloVidaActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate chamado!");
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart chamado!");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume chamado!");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause chamado!");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop chamado!");
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart chamado!");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy chamado!");
+        super.onDestroy();
+    }
+}
--- PIM/app/src/main/java/br/unip/ads/pim/MainActivity.java
+++ PIM/app/src/main/java/br/unip/ads/pim/MainActivity.java
@@ -1,57 +1,12 @@
 package br.unip.ads.pim;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = MainActivity.class.getSimpleName();
+public class MainActivity extends CicloVidaActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Log.d(TAG, "onCreate chamado!");
-    }
-
-    @Override
-    protected void onStart() {
-        Log.d(TAG, "onStart chamado!");
-        super.onStart();
-    }
-
-    @Override
-    protected void onResume() {
-        Log.d(TAG, "onResume chamado!");
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        Log.d(TAG, "onPause chamado!");
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        Log.d(TAG, "onStop chamado!");
-        super.onStop();
-    }
-
-    @Override
-    protected void onRestart() {
-        Log.d(TAG, "onRestart chamado!");
-        super.onRestart();
-    }
-
-    @Override
-    protected void onDestroy() {
-        Log.d(TAG, "onDestroy chamado!");
-        super.onDestroy();
     }
 }
